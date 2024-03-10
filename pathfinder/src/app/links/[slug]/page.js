c549eb0ed9bf5ddc2f1051e09b8144895815a0a9
unknown <dejanmaric259@gmail.com>
2024-03-10 14:57:32 +0100

    sign out functionality

--- pathfinder/src/app/links/[slug]/page.js
+++ pathfinder/src/app/links/[slug]/page.js
@@ -1,14 +1,35 @@
+'use client'
 import CreateLink from '@/app/components/CreateLink/CreateLink'
 import LinkComponent from '@/app/components/LinkComponent/LinkComponent'
 import Heading from '@/app/components/common/Heading/Heading'
 import React from 'react'
-
+import {app,auth,} from '@/app/firebase';
+import { signOut } from 'firebase/auth';
+import { useRouter } from 'next/navigation'
 
 function Links() {
+  const user = auth.currentUser;
+  const router = useRouter()
+if (user) {
+  console.log(user, 'asdasd')
+} else {
+  console.log('no suer')
+}
+
+function handleSignOut() {
+  signOut(auth).then(() => {
+    console.log('Signed out');
+    router.push('/')
+  }).catch((error) => {
+    console.log(error);
+  });
+}
+
   return (
     <div className='flex flex-col gap-4 justify-center items-center h-full'>
         <Heading text='Jon Doe Links'/>
         <LinkComponent text='Follow my facebook' link='https://www.facebook.com'/>
+        <button onClick={()=>handleSignOut()}>Sign out</button>
         <CreateLink/>
     </div>
   )
